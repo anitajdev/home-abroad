a8d3e2a981a39a7c021c71a8809cb6cb7b2208f1
Anita <jelicanita7@gmail.com>
2023-10-15 20:25:58 +0200

    Revert "Carousel pictures"
    This reverts commit 98627edc303fb8668ce8b53b99494e9034a3f436.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,11 @@
 import './App.css'
 import Nav from './components/Nav'
-import Hero from './sections/Hero'
 
 const App = () => (
 
   <main className="relative">
-      <Nav/>
-    <section className="xl:padding-l wide: padding-r padding-b">
-      <Hero/>
-    </section>
-
-
-    
+    <Nav/>
   </main>
-
   
 )
 
