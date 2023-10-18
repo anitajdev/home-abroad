c1548e9b52b6bdd909818e4abcd18eb07143a9c9
Anita <jelicanita7@gmail.com>
2023-10-18 17:18:46 +0200

    Login and Registration - connecting to the db
    Connecting to MongoDB.

--- api/index.js
+++ api/index.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+require('dotenv').config();
 const app = express();
+
+app.use(express.json());
 
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173'
 }));
 
+mongoose.connect(process.env.MONGO_URL);
+
+
 app.get('/test', (req, res) => {
-    res.json("da li ovo vise radiiii!!!???");
+    res.json("test ok");
+});
+
+
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body;
+    
+
+
+    res.json({name, email, password});
 });
 
 app.listen(4000);
+
+
+
+
+
+//saM73Eu5MuNtAM4o - MAIN
+// KAlA4dFNdgYaMmBi - home-abroad-booking-app
+
