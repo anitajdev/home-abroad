0ec58416b8b093d07d32ef96c6cc8aa81ee9232c
Anita <jelicanita7@gmail.com>
2023-10-18 17:18:46 +0200

    Registration part

--- api/index.js
+++ api/index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+require('dotenv').config();
 const app = express();
+
+app.use(express.json());
 
 app.use(cors({
     credentials: true,
     origin: 'http://localhost:5173'
 }));
 
+mongoose.connect(process.env.MONGO_URL);
+
+
 app.get('/test', (req, res) => {
-    res.json("da li ovo vise radiiii!!!???");
+    res.json("test ok");
+});
+
+
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body;
+    
+
+
+    res.json({name, email, password});
 });
 
 app.listen(4000);
+
+
+
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -9,7 +9,11 @@ export default function RegisterPage() {
   const [password, setPassword] = useState("");
   function registerUser(ev) {
     ev.preventDefault();
-    axios.get('/test');
+    axios.post('/register', {
+      name,
+      email,
+      password
+    });
   }
 
     return (
