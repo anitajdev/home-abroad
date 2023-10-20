ff27d93c1624723245b3688b35150e7d07402110
Anita <jelicanita7@gmail.com>
2023-10-20 20:30:56 +0200

    Account Page - update
    Adding more features.

--- api/index.js
+++ api/index.js
@@ -80,6 +80,16 @@ app.get('/profile', (req, res) => {
     res.json({token});
 });
 
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json(true);
+});
+
+
+
+
+
+
+
 app.listen(4000);
 
 
