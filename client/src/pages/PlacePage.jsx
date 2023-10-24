01f83c9798fb87e3cd0a0fb7de915846e51e5dfb
Anita <jelicanita7@gmail.com>
2023-10-24 22:04:00 +0200

    Single place page
    Description, booking section and extra info.

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import BookingWidget from "../BookingWidget";
 
 
 const PlacePage = () => {
@@ -26,7 +27,7 @@ const PlacePage = () => {
         <div className="absolute inset-0 bg-black text-white min-h-screen">
             <div className="bg-black p-8 grid gap-4">
                 <div>
-                    <h2 className="text-3xl">Photos of {place.title}</h2>
+                    <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
                     <button onClick={() => setShowAllPhotos(false)} className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -45,7 +46,7 @@ const PlacePage = () => {
   }
 
   return (
-    <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
+    <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
         <h1 className="text-3xl">{place.title}</h1>
         <a className="flex gap-1 my-3 block font-semibold underline" target="_blank" href={"https://maps.google.com/?q="+place.address}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -59,18 +60,18 @@ const PlacePage = () => {
                 <div>
                     {place.photos?.[0] && (
                         <div>
-                            <img className="aspect-square object-cover" src={"http://localhost:4000/uploads/"+place.photos[0]} alt=""/>
+                            <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={"http://localhost:4000/uploads/"+place.photos[0]} alt=""/>
                         </div>
                     )}
                 </div>
                 <div className="grid">
                     {place.photos?.[1] && (
-                        <img className="aspect-square object-cover" src={"http://localhost:4000/uploads/"+place.photos[1]} alt=""/>
+                        <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={"http://localhost:4000/uploads/"+place.photos[1]} alt=""/>
                     )}
 
                     <div className="overflow-hidden">
                     {place.photos?.[2] && (
-                        <img className="aspect-square object-cover relative top-2" src={"http://localhost:4000/uploads/"+place.photos[2]} alt=""/>
+                        <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover relative top-2" src={"http://localhost:4000/uploads/"+place.photos[2]} alt=""/>
                     )}
                     </div>
                 </div>
@@ -82,8 +83,29 @@ const PlacePage = () => {
                 Show more photos
             </button>
         </div>
+       
+        <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
+            <div>
+                <div className="my-4">
+                    <h2 className="font-semibold text-2xl">Description</h2>
+                    {place.description}
+                </div>
+                Check in: {place.checkIn} <br />
+                Check out: {place.checkOut} <br />
+                Max number of guests: {place.maxGuests}
+            </div>
+            <div>
+                <BookingWidget place={place} />
+            </div>
+        </div>
+        <div className="bg-white -mx-8 px-8 py-8 border-t">
+            <div>
+                <h2 className="font-semibold text-2xl">Extra info</h2> 
+            </div>
+            <div className="mb-4 mt-2 text-sm text-gray-700 leading-5">{place.extraInfo}</div>
+        </div>
     </div>
-  )
+  );
 }
 
 export default PlacePage
