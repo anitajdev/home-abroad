bdf9334c1956365b45c0999a3a73508bf1161822
Anita <jelicanita7@gmail.com>
2023-10-24 20:41:29 +0200

    Single place page
    Title, address, photos.
    Show more photos button

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState} from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+
 
 const IndexPage = () => {
   const [places, setPlaces] = useState([]);
@@ -14,7 +16,7 @@ const IndexPage = () => {
   return (
     <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {places.length > 0 && places.map(place => (
-        <div>
+        <Link to={'/place/'+place._id}>
           <div className="bg-gray-500 mb-2 rounded-2xl flex">
             {place.photos?.[0] && (
               <img className="rounded-2xl object-cover aspect-square" src={"http://localhost:4000/uploads/" +place.photos?.[0]} alt="" />
@@ -25,7 +27,7 @@ const IndexPage = () => {
           <div className="mt-1">
             <span className="font-bold">${place.price}</span> per night
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
