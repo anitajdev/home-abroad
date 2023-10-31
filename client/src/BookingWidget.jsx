15373bd6e6daa3b75d2b6ecacd4655d4ba30e89d
Anita <jelicanita7@gmail.com>
2023-10-31 16:55:01 +0100

    Perks and other details update

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -39,11 +39,11 @@ const BookingWidget = ({place}) => {
         return <Navigate to={redirect} />
     }
 
-    
+
   return (
     <div className="bg-white shadow p-4 rounded-2xl">
         <div className="text-2xl text-center">
-            Price: ${place.price} / per night
+            Price: {place.price}€ / per night
         </div>
         <div className="border rounded-2xl mt-4">
             <div className="flex">
